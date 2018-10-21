b6cc32598fde50309bc857d6ce448f10d99c0764
Yingying Chen <cyy870119@gmail.com>
2018-10-21 12:02:13 -0400

    add GET:/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var { ObjectID } = require('mongodb');
+
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -30,7 +32,30 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.get('/todos/:id', (req, res) => {
 
-app.listen(3000, () => {
-  console.log('started on port 3000');
-})
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=> {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+
+
+}, (e) => {
+  res.status(400).send(e);
+}),
+
+
+  app.listen(3000, () => {
+    console.log('started on port 3000');
+  })
