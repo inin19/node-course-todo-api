bbe028cdd25d9d7ba65f2f3b5f69d2a6f4ea3fac
Yingying Chen <cyy870119@gmail.com>
2018-10-21 18:30:58 -0400

    yeah

--- server/models/user.js
+++ server/models/user.js
@@ -70,6 +70,28 @@ userSchema.statics.findByToken = function (token) {
 
 };
 
+userSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    // bcrypt only support call back
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      })
+    })
+  })
+}
+
+
 
 userSchema.pre('save', function (next) {
   var user = this;
@@ -84,6 +106,7 @@ userSchema.pre('save', function (next) {
     next();
   }
 })
+
 
 
 var User = mongoose.model('User', userSchema);
